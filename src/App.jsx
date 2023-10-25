3bbd928fca591985cdce92f8f7f962866f111bfb
tmassam <101155429+tmassam@users.noreply.github.com>
2023-10-25 13:41:50 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import "./app.scss";
+import Contact from "./components/Contact/Contact";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Parallax from "./components/Parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
 export default App;
